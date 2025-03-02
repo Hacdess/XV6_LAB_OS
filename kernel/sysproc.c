fc70a8549ce05927f9eed9946489a1c11b4245d5
TKin <letrungkienthd@gmail.com>
2025-03-02 17:16:33 +0700

    try: testhello

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,8 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int sys_hello(void) { 
+  printf("Hello, world!\n"); 
+  return 0; 
+} 
--- user/user.h
+++ user/user.h
@@ -1,3 +1,4 @@
+#include "kernel/types.h"
 struct stat;
 
 // system calls
@@ -22,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int hello(void); 
 
 // ulib.c
 int stat(const char*, struct stat*);
